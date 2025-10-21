b2bce95d4ce7acb340871f62489b7a7a7e564b9c
Gokul <gokul@Gokuls-MacBook-Pro.local>
2025-10-20 20:20:42 -0700

    Add test function

---
+++ netlify/functions/test.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      success: true,
+      message: 'Test function working!',
+      timestamp: new Date().toISOString()
+    })
+  };
+};
